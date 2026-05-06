89deff6874315c066da82e1c6094dee5da3ddbe7
Mirya <xmirya@gmail.com>
2026-05-06 23:39:52 +0200

    Dumb down PrimitiveJob to the main thread (threads are disabled in our env due to sandboxing approach)

--- qiskit/primitives/primitive_job.py
+++ qiskit/primitives/primitive_job.py
@@ -13,8 +13,9 @@
 Job for the reference implementations of Primitives V1 and V2.
 """
 
+# PrivimiteJob is dumbed down to run on the main thread (threading isn't enabled in our environment for security reasons)
+
 import uuid
-from concurrent.futures import ThreadPoolExecutor
 
 from qiskit.providers import JobError, JobStatus
 from qiskit.providers.jobstatus import JOB_FINAL_STATES
@@ -40,68 +41,36 @@ class PrimitiveJob(BasePrimitiveJob[ResultT, JobStatus]):
             kwargs: any additional keyword arguments
         """
         super().__init__(str(uuid.uuid4()))
-        self._future = None
-        self._function = function
+        self._submitted = False
         self._result = None
-        self._status = None
+        self._function = function
         self._args = args
         self._kwargs = kwargs
 
     def _submit(self):
-        if self._future is not None:
-            raise JobError("Primitive job has been submitted already.")
-
-        executor = ThreadPoolExecutor(max_workers=1)
-        self._future = executor.submit(self._function, *self._args, **self._kwargs)
-        executor.shutdown(wait=False)
-
-    def __getstate__(self):
-        _ = self.result()
-        _ = self.status()
-        state = self.__dict__.copy()
-        state["_future"] = None
-        return state
-
-    def __setstate__(self, state):
-        self.__dict__.update(state)
-        self._future = None
+        self._result = self._function(*self._args, **self._kwargs)
+        self._submitted = True
 
     def result(self) -> ResultT:
-        if self._result is None:
-            self._check_submitted()
-            self._result = self._future.result()
         return self._result
 
     def status(self) -> JobStatus:
-        if self._status is None:
-            self._check_submitted()
-            if self._future.running():
-                # we should not store status running because it is not completed
-                return JobStatus.RUNNING
-            elif self._future.cancelled():
-                self._status = JobStatus.CANCELLED
-            elif self._future.done() and self._future.exception() is None:
-                self._status = JobStatus.DONE
-            else:
-                self._status = JobStatus.ERROR
-        return self._status
-
-    def _check_submitted(self):
-        if self._future is None:
-            raise JobError("Primitive Job has not been submitted yet.")
+        if self._submitted:
+            return JobStatus.DONE
+        raise JobError("Primitive Job has not been submitted yet.")
 
     def cancel(self):
-        self._check_submitted()
-        return self._future.cancel()
+        pass
 
     def done(self) -> bool:
-        return self.status() == JobStatus.DONE
+        self.status()
+        return True
 
     def running(self) -> bool:
-        return self.status() == JobStatus.RUNNING
+        return False
 
     def cancelled(self) -> bool:
-        return self.status() == JobStatus.CANCELLED
+        return False
 
     def in_final_state(self) -> bool:
-        return self.status() in JOB_FINAL_STATES
+        return self.done()
